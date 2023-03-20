4cbf00a1e5482df04a20a9fad53a7f7168e57a3d
JohnCorby <williamforgames6@gmail.com>
2023-03-19 23:01:02 -0700

    make force applier thruster thing

---
+++ GmodThrusters/Thruster.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace GmodThrusters;
+
+/// <summary>
+/// applies thrust to the attached body
+/// </summary>
+public class Thruster : MonoBehaviour
+{
+	private OWRigidbody _body;
+
+	public float Thrust = 10;
+
+	private void Awake()
+	{
+		_body = this.GetAttachedOWRigidbody();
+	}
+
+	private void FixedUpdate()
+	{
+		var force = transform.forward * Thrust;
+
+		if (_body.RunningKinematicSimulation())
+		{
+			// _kinematicRigidbody.AddForceAtPosition does nothing
+			_body._kinematicRigidbody.AddForce(force, ForceMode.Acceleration);
+		}
+		else
+		{
+			_body._rigidbody.AddForceAtPosition(force, transform.position, ForceMode.Acceleration);
+		}
+	}
+}
